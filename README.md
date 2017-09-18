d9c621cf9987d0e877a8de237398a93b799a27a0
Dan Norman <normande@pkcsecurity.com>
2017-09-18 11:59:29 -0700

    backend scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sirhaxalot
-Automated 2fa spear phishing
+
+FIXME
+
+## Prerequisites
+
+You will need [Leiningen][] 2.0.0 or above installed.
+
+[leiningen]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+## License
+
+Copyright © 2017 FIXME
---
+++ server/project.clj
@@ -0,0 +1,12 @@
+(defproject sirhaxalot "0.1.0-SNAPSHOT"
+  :description "An Automated Spearfishing tool for users with 2fa enabled"
+  :url "http://sirhaxalot.com"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler sirhaxalot.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
